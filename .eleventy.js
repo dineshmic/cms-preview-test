8c8a5442c0230511b10c23476d2bdc83e89f4592
dineshmic <dinesh.mic@gmail.com>
2023-09-11 20:50:36 +0530

    for cms

---
+++ .eleventy.js
@@ -0,0 +1,21 @@
+const { DateTime } = require('luxon'); 
+
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy('./src/assets');
+    eleventyConfig.addPassthroughCopy('./src/admin');
+    
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+        // Parse the date string directly using Luxon's DateTime
+        const parsedDate = DateTime.fromISO(dateObj); 
+        
+        // Format the parsed date as a localized date string
+        return parsedDate.toLocaleString(DateTime.DATE_MED);
+    });
+    	
+	return {
+		dir: {
+			input: "src",
+			output: "public"
+		}
+	};
+}
